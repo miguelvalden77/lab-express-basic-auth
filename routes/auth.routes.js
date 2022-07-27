8f1694db3c71f6551b01dcbb31f361155a3f13b7
manuetov <manuetov@hotmail.com>
2022-07-27 18:02:33 +0200

    2 iteracion

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,5 +18,36 @@ router.post("/signup", async (req, res, next)=>{
     res.redirect("/auth/login")
 })
 
+router.get('/login', (req, res, next)=>{
+    res.render('auth/login')
+})
+
+router.post('/login', async (req, res, next)=>{
+    try {
+        const {username, password} = req.body
+        const foundUser = await User.findOne({username})
+        const validatedPassword = await bcrypt.compare(password, foundUser.password)
+        console.log(validatedPassword)
+
+        req.session.user = {
+            _id: foundUser._id,
+            email: foundUser.email,
+            username: foundUser.username
+        }
+
+        req.session.save(() => {
+            res.redirect('/')
+        })
+        console.log('algo');
+    } catch (error) {
+        next(error)
+    }
+
+
+
+
+    
+
+})
 
 module.exports = router
